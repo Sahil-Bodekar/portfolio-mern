686d00b8d19e4d1f1d847a0e05cdd51a5e17baf1
Sahil Bodekar <sahilbbodekar1999@gmail.com>
2026-03-05 00:40:02 +0530

    Updated backend URL to production

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -24,7 +24,7 @@ function Contact() {
     setStatus("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://portfolio-mern-2-85cx.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
